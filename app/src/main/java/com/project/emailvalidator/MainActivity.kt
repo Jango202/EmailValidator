257bc33e7759e40497be8cbbda8a91448ee6f653
Jaison-Abraham <jaisonabraham8606@gmail.com>
2025-07-30 20:39:21 -0400

    MainActivity

--- app/src/main/java/com/project/emailvalidator/MainActivity.kt
+++ app/src/main/java/com/project/emailvalidator/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.project.emailvalidator
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editTextUserInput = findViewById<EditText>(R.id.editTextUserInput)
+        val buttonChangeText = findViewById<Button>(R.id.buttonChangeText)
+        val buttonOpenActivityAndChangeText = findViewById<Button>(R.id.buttonOpenActivityAndChangeText)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        buttonChangeText.setOnClickListener {
+            val input = editTextUserInput.text.toString()
+            textView.text = input
+        }
+
+        buttonOpenActivityAndChangeText.setOnClickListener {
+            val input = editTextUserInput.text.toString()
+            val intent = Intent(this, ShowTextActivity::class.java)
+            intent.putExtra("user_input", input)
+            startActivity(intent)
         }
     }
 }
